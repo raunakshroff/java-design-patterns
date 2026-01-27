038b41c1df574138cacb60b8116ef2c331d2b06e
Raunak Shroff <raunak.shroff@gmail.com>
2026-01-27 10:45:21 +0000

    Pending changes exported from your codespace

---
+++ src/main/java/com/example/RoomDecorator.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public abstract class RoomDecorator implements Room {
+
+  protected Room room;
+
+  public RoomDecorator(Room room) {
+    this.room = room;
+  }
+
+  @Override
+  public void printFurniture() {
+    System.out.println("Room with basic furniture");
+  }
+
+}
